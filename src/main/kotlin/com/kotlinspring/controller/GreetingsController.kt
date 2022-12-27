662b166dfcf1cd29d7b75f0a5e74c05928205fe1
bhagyashree.vittal <bhagyashreegowri@gmail.com>
2022-12-27 10:18:32 +0100

    kotlin springboot - simple controller, service class example

---
+++ src/main/kotlin/com/kotlinspring/controller/GreetingsController.kt
@@ -0,0 +1,21 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.service.GreetingsService
+import mu.KotlinLogging
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/greetings")
+class GreetingsController(val greetingsService: GreetingsService) {
+
+    private val logger = KotlinLogging.logger {}
+
+    @GetMapping("/{name}")
+    fun requestGreetings(@PathVariable("name") name: String): String{
+        logger.info { "Name is: $name" }
+        return greetingsService.retrieveGreeting(name)
+    }
+}
